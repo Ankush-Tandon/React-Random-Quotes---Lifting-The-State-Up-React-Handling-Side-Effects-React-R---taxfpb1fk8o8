b8c9f311fed1aed5084262c4cbe88866ad83998b
Ankush Tandon <122861187+Ankush-Tandon@users.noreply.github.com>
2023-11-24 19:30:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 var colors = [
@@ -17,14 +17,39 @@ var colors = [
 ];
 
 const App = () => {
+  const [quote, setQuote] = useState({});
+  const [colorIndex, setColorIndex] = useState(0);
 
-    return (
-      <div id="main">
-        <div id="wrapper">
-          
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setQuote(data);
+      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    } catch (error) {
+      console.error("Error fetching random quote:", error);
+    }
+  };
+
+  const quoteStyle = {
+    backgroundColor: colors[colorIndex],
+  };
+
+  return (
+    <div id="main">
+      <div id="wrapper" style={quoteStyle}>
+        <div className="quote-text">
+          {quote.content}
         </div>
+        <div className="quote-author">
+          {quote.author}
+        </div>
+        <button id="new-quote" className="button" onClick={fetchRandomQuote}>
+          New Quote
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default App;
